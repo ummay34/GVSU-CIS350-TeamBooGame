49d15f524b2af86f3615e713d8b538ae1c79651b
Jake Irons <jakeirons007@gmail.com>
2020-10-14 02:20:26 -0400

    adding level loader

--- src/TeamBooGame/Assets/FreeHorrorGameKit/Scripts/PlayerBehaviour.cs
+++ src/TeamBooGame/Assets/FreeHorrorGameKit/Scripts/PlayerBehaviour.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.Remoting.Messaging;
+/*using System.Runtime.Remoting.Messaging;*/
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
